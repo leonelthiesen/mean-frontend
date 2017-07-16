78adf8cea9b83c02a12e8f9266b93c74e146a3db
Leonel Thiesen <leonelthiesen@gmail.com>
2017-07-16 19:56:05 -0300

    refactory dashboard

--- app/dashboard/dashboardController.js
+++ app/dashboard/dashboardController.js
@@ -1,20 +1,25 @@
-angular.module('MEAN').controller('DashboardCtrl', [
-	'$scope',
-	'$http',
-	DashboardController
-]);
+(function() {
 
-function DashboardController ($scope, $http) {
-	$scope.getSummary = function() {
-		const url = 'http://localhost:3003/api/billingSummary';
+	angular.module('MEAN').controller('DashboardCtrl', [
+		'$http',
+		DashboardController
+	]);
 
-		$http.get(url).then(function(response) {
-			const { credit = 0, debt = 0 } = response.data;
-			$scope.credit = credit;
-			$scope.debt = debt;
-			$scope.total = credit - debt;
-		});
+	function DashboardController ($http) {
+		const vm = this;
+
+		vm.getSummary = function() {
+			const url = 'http://localhost:3003/api/billingSummary';
+
+			$http.get(url).then(function(response) {
+				const { credit = 0, debt = 0 } = response.data;
+				vm.credit = credit;
+				vm.debt = debt;
+				vm.total = credit - debt;
+			});
+		}
+
+		vm.getSummary();
 	}
 
-	$scope.getSummary();
-}
+})()
