7247dad63170b17e1d27fb2ff6ef035c0c96c381
Leonel Thiesen <leonelthiesen@gmail.com>
2017-07-18 22:39:17 -0300

    mais ajustes no cadastro de ciclos de pagamentos

--- app/common/components/field.js
+++ app/common/components/field.js
@@ -6,7 +6,8 @@
 			grid: '@',
 			placeholder: '@',
 			type: '@',
-			model: '='
+			model: '=',
+			readonly: '<'
 		},
 		controller: [
 			'gridSystem',
@@ -18,7 +19,7 @@
 			<div class="{{ $ctrl.gridClasses }}">
 				<div class="form-group">
 					<label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
-					<input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}" type="{{ $ctrl.type }}" ng-model="$ctrl.model">
+					<input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}" type="{{ $ctrl.type }}" ng-model="$ctrl.model" ng-readonly="$ctrl.readonly">
 				</div>
 			</div>
 		`
